e4814bda7fb51442638c48d2ed6af74244bdb8c7
wdd119 <1240172019@qq.com>
2018-11-07 23:30:57 +0800

    删除 readMe 添加笔记

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 
 import Koa from 'koa'
-const consola = require('consola')
-const { Nuxt, Builder } = require('nuxt')
+import consola from 'consola'
+import { Nuxt, Builder } from 'nuxt'
 
 const app = new Koa()
 const host = process.env.HOST || '127.0.0.1'
